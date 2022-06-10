4ba5d8bc8931712a473bd4b7b99cf670d618cf29
B4K35 <107258769+B4K35@users.noreply.github.com>
2022-06-10 23:27:29 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 20;
+
+function toggleContrast() {
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList += " dark-theme"
+  }
+  else {
+    document.body.classList.remove("dark-theme")
+  }
+}
+
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
+}
